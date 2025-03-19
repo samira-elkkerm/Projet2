b338ce2639920cf680f702ba82a883c2c71baba2
nouhailaghanjiri <nouhailaghanjiri@gmail.com>
2025-03-19 03:25:08 +0100

    Afficher les catégories avec des liens vers la page boutique

--- frontend/src/composants/Client/BarreCategories.js
+++ frontend/src/composants/Client/BarreCategories.js
@@ -2,11 +2,12 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCategories } from '../../redux/actions/categoriesActions';
 import '../../App.css';
-
+import { Link } from 'react-router-dom';
 
 const BarreCategories = () => {
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.categories.categories); 
+
   useEffect(() => {
     dispatch(fetchCategories());
   }, [dispatch]);
@@ -18,8 +19,9 @@ const BarreCategories = () => {
         {categories && categories.length > 0 ? (
           categories.map((categorie) => (
             <li key={categorie.id}>
+              <Link to={`/boutique?categorie=${categorie.id}`} style={{ color: 'black', textDecoration: 'none' }}>
+              {categorie.type} </Link>
               
-              {categorie.type} 
               
               
             </li>
